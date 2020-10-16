4486bcbc3540d45c0032cb645a9f46164d9deafd
Gareth Field <field.gareth@gmail.com>
2020-10-16 08:11:19 -0400

    push

--- mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
+++ mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -1,14 +1,21 @@
-
 public class Printer {
-
     public static void main(String[] args) {
-        // You can test the method here
-        int[] array = {5, 1, 3, 4, 2};
+        int[] array = { 5, 1, 3, 4, 2 };
         printArrayInStars(array);
     }
 
     public static void printArrayInStars(int[] array) {
-        // Write some code in here
+        int index = 0;
+        while (index < array.length) {
+            for (int i = 1; i <= array[index]; i++) {
+                if (i == array[index]) {
+                    System.out.print("*\n");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            index++;
+        }
     }
 
 }
