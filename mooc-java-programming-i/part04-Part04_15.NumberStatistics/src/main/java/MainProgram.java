7c06e8eaf5c7ea375cdf020d7754ee1b47dbd2bf
Gareth Field <field.gareth@gmail.com>
2020-10-19 11:57:39 -0400

    push

--- mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -1,16 +1,40 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
-
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        System.out.println("Enter numbers:");
+        boolean exitSignalled = false;
+        ArrayList<Integer> evens = new ArrayList<>();
+        ArrayList<Integer> odds = new ArrayList<>();
+        while (!exitSignalled) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == -1) {
+                exitSignalled = true;
+                continue;
+            }
+            if (number % 2 == 0) {
+                evens.add(number);
+            } else {
+                odds.add(number);
+            }
+        }
+        Statistics totalStats = new Statistics();
+        Statistics evenStats = new Statistics();
+        Statistics oddStats = new Statistics();
+        for (int even : evens) {
+            totalStats.addNumber(even);
+            evenStats.addNumber(even);
+        }
+        for (int odd : odds) {
+            totalStats.addNumber(odd);
+            oddStats.addNumber(odd);
+        }
+        System.out.println("Sum: " + totalStats.sum());
+        System.out.println("Sum of even numbers: " + evenStats.sum());
+        System.out.println("Sum of odd numbers: " + oddStats.sum());
     }
 }
--- mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,29 @@
 
 public class Statistics {
+  private int count;
+  private int sum;
+
+  // public Statistics() {
+  // this.count = 0;
+  // }
+
+  public void addNumber(int number) {
+    sum += number;
+    count++;
+  }
+
+  public int getCount() {
+    return count;
+  }
+
+  public int sum() {
+    return sum;
+  }
+
+  public double average() {
+    if (count == 0) {
+      return 0.0;
+    }
+    return (double) sum / count;
+  }
 }
