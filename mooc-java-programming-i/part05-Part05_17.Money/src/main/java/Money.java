3ebb1f262320f97f8f8a5c8adc5c221463eae634
Gareth Field <field.gareth@gmail.com>
2020-10-31 06:32:25 -0400

    push

--- mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
+++ mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
@@ -32,4 +32,35 @@ public class Money {
         return this.euros + "." + zero + this.cents + "e";
     }
 
+    public Money plus(Money addition) {
+        int newEuros = this.euros + addition.euros();
+        int newCents = this.cents + addition.cents();
+        Money newMoney = new Money(newEuros, newCents);
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros == compared.euros()) {
+            return this.cents < compared.cents();
+        }
+        return this.euros < compared.euros();
+    }
+
+    public Money minus(Money decreaser) {
+        int newCents = this.cents - decreaser.cents();
+        int newEuros = this.euros - decreaser.euros();
+        if (newCents < 0) {
+            if (newEuros > 1) {
+                newEuros -= 1;
+                newCents = 100 + newCents;
+            } else {
+                return new Money(0, 0);
+            }
+        }
+        if (newEuros < 0) {
+            return new Money(0, 0);
+        }
+        return new Money(newEuros, newCents);
+    }
+
 }
