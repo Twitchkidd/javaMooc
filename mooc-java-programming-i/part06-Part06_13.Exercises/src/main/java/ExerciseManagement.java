137d1726db39098c9eb385ecfaa8a7b07e2bd41b
Gareth Field <field.gareth@gmail.com>
2020-11-09 16:31:53 -0500

    push

--- mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
 
 public class ExerciseManagement {
+  private ArrayList<Exercise> exercises;
 
+  public ExerciseManagement() {
+    this.exercises = new ArrayList<>();
+  }
+
+  public ArrayList<String> exerciseList() {
+    ArrayList<String> list = new ArrayList<String>();
+    for (Exercise exercise : this.exercises) {
+      list.add(exercise.getName());
+    }
+    return list;
+  }
+
+  public void add(String exerciseName) {
+    this.exercises.add(new Exercise(exerciseName));
+  }
+
+  public void markAsCompleted(String exerciseName) {
+    for (Exercise exercise : this.exercises) {
+      if (exercise.getName().equals(exerciseName)) {
+        exercise.setCompleted(true);
+      }
+    }
+  }
+
+  public boolean isCompleted(String exerciseName) {
+    for (Exercise exercise : this.exercises) {
+      if (exercise.getName().equals(exerciseName)) {
+        return exercise.isCompleted();
+      }
+    }
+    return false;
+  }
 }
--- mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,48 @@
-
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+import org.junit.Before;
 
 public class ExerciseManagementTest {
+  private ExerciseManagement mgmt;
 
+  @Before
+  public void initialize() {
+    mgmt = new ExerciseManagement();
+  }
+
+  @Test
+  public void exerciseListEmptyAtBeginning() {
+    ExerciseManagement mgmt = new ExerciseManagement();
+    assertEquals(0, mgmt.exerciseList().size());
+  }
+
+  @Test
+  public void addingExerciseGrowsListByOne() {
+    ExerciseManagement mgmt = new ExerciseManagement();
+    mgmt.add("Write a test");
+    assertEquals(1, mgmt.exerciseList().size());
+  }
+
+  @Test
+  public void addedExerciseIsInList() {
+    ExerciseManagement mgmt = new ExerciseManagement();
+    mgmt.add("Write a test");
+    assertTrue(mgmt.exerciseList().contains("Write a test"));
+  }
+
+  @Test
+  public void exerciseCanBeMarkedAsCompleted() {
+    mgmt.add("New exercise");
+    mgmt.markAsCompleted("New exercise");
+    assertTrue(mgmt.isCompleted("New exercise"));
+  }
+
+  @Test
+  public void ifNotMarkedCompletedIsNotCompleted() {
+    mgmt.add("New exercise");
+    mgmt.markAsCompleted("New exercise");
+    assertFalse(mgmt.isCompleted("Some exercise"));
+  }
 }
