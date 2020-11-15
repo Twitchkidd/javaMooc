5b269481dd04c397f2badc6b6e45973020604f0b
Gareth Field <field.gareth@gmail.com>
2020-11-15 15:54:12 -0500

    push

--- mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,12 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            String answer = scanner.nextLine();
+            if (answer.equals("end"))
+                break;
+            int cubeMe = Integer.valueOf(answer);
+            System.out.println((int) Math.pow((double) cubeMe, (double) 3));
+        }
     }
 }
